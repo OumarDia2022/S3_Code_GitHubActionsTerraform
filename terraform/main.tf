85b0c4cfbb4e72f0b8463b095a314f77d6b1ad0c
OumarDia2022 <101050406+OumarDia2022@users.noreply.github.com>
2024-11-14 16:29:11 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,10 +35,6 @@ resource "azurerm_linux_web_app" "website" {
   location            = var.location
   resource_group_name = data.azurerm_resource_group.wsdevops.name
   service_plan_id     = azurerm_service_plan.sp1.id
-  site_config {
-    linux_fx_version = "NODE|22-lts"
-    #scm_type         = "LocalGit"
-  }
 }
 resource "azurerm_log_analytics_workspace" "log" {
   name                = "odia24-lg-analytics"
