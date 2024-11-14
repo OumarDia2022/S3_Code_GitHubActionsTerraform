f61810e600bebdde7cf1a245c2cb873da9d651f6
OumarDia2022 <101050406+OumarDia2022@users.noreply.github.com>
2024-11-14 11:48:13 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,14 +71,14 @@ resource "null_resource" "link_monitoring" {
     EOT
     environment = {
       // Parameters needed to login
-      con_client_id     = var.AZURE_AD_CLIENT_ID
-      con_client_secret = var.AZURE_AD_CLIENT_SECRET
-      con_tenant_id     = var.AZURE_AD_TENANT_ID
+      con_client_id     = var.client_id
+      con_client_secret = var.client_secret
+      con_tenant_id     = var.tenant_id
       // Parameters needed for linking
       inst_key          = azurerm_application_insights.appi.instrumentation_key
-      conn_str          = azurerm_application_insights.appi.connection_string      
-      rg_name           = var.RG
-      web_app_name      = webappodia24
+      conn_str          = azurerm_application_insights.appi.connection_string            
+      rg_name           = var.rg_name
+      web_app_name      = var.web_app_name
     }
   }
 }
